bb729603dbc9f3bc5f4487c41a960ef79c16a022
Alexandre Erich Sébastien Georges <aes.georges@gmail.com>
2022-08-11 12:01:55 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# Original MATLAB Water Column Master Code from Lisa Lucas, modified by Tina Chow. Spring 2018: Mark Stacey and Michaella Chung.
+# Code adapted to Python and for USGS by Alexandre E. S. Georges, Environmental Engineering PhD Student at UC Berkeley - 2022
+
